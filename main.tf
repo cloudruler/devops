837c9dd977077ea5671b154fe190168fc82b9a64
Brian Moore <brianmoore2@outlook.com>
2021-03-16 09:57:37 -0500

    Added count flag

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "cr" {
+  count                    = 1
   name                     = "cloudruler"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
---
+++ outputs.tf
@@ -0,0 +1,7 @@
+# output "appcs_id" {
+#   value = azurerm_app_configuration.appcs.id
+# }
+
+# output "appcs_endpoint" {
+#   value = azurerm_app_configuration.appcs.endpoint
+# }
