16f87c828d95c1cc729bbec229d76e72b5f5f94c
Brian Moore <brian.moore@cloudruler.io>
2021-04-05 14:57:49 -0500

    Added SPN to output

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
-# output "devops_azurecr_service_principal_id" {
-#   value = azurerm_app_configuration.appcs.id
-# }
+output "devops_azurecr_service_principal_id" {
+  value = azuredevops_serviceendpoint_azurecr.cloudruler.service_principal_id
+}
 
 # output "appcs_endpoint" {
 #   value = azurerm_app_configuration.appcs.endpoint
