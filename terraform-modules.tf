1cbdfc4b01e9e10c2ad7f3764afa587955d1210e
Brian Moore <brianmoore@cloudruler.com>
2022-03-21 21:23:05 +0000

    Fixed modules

--- terraform-modules.tf
+++ terraform-modules.tf
@@ -1,6 +1,6 @@
-resource "github_repository" "terraform_github_aks_node" {
-  name        = "terraform-github-aks_node"
-  description = "Module for creating an AKS node"
+resource "github_repository" "terraform_azurerm_aks_cluster" {
+  name        = "terraform-azurerm-aks_cluster"
+  description = "Module for creating an AKS cluster"
   auto_init   = true
 
   template {
@@ -9,3 +9,10 @@ resource "github_repository" "terraform_github_aks_node" {
   }
 }
 
+resource "tfe_registry_module" "module-terraform_azurerm_aks_cluster" {
+  vcs_repo {
+    display_identifier = "${var.github_organization}/${github_repository.terraform_azurerm_aks_cluster.name}"
+    identifier         = "${var.github_organization}/${github_repository.terraform_azurerm_aks_cluster.name}"
+    oauth_token_id     = tfe_oauth_client.github_cloudruler.oauth_token_id
+  }
+}
