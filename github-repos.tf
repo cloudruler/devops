3e6b3804da683743c6f2a0558d844c153791a137
Brian Moore <brianmoore@cloudruler.com>
2022-05-01 20:26:48 +0000

    Adding AWS stuff

---
+++ github-repos.tf
@@ -0,0 +1,91 @@
+data "github_organization" "org" {
+    name = var.github_organization
+}
+
+resource "github_repository" "devops" {
+  name        = "devops"
+  description = "Terraform for devops"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository" "infrastructure" {
+  name        = "infrastructure"
+  description = "Terraform for infrastructure"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository" "connectivity" {
+  name        = "connectivity"
+  description = "Terraform for connectivity"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository" "identity" {
+  name        = "identity"
+  description = "Terraform for identity"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository" "images" {
+  name        = "images"
+  description = "Terraform for images"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository" "initialization" {
+  name        = "initialization"
+  description = "Terraform for initialization"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository" "sandbox" {
+  name        = "sandbox"
+  description = "Terraform for sandbox"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository" "scripts" {
+  name        = "scripts"
+  description = "Terraform for scripts"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository" "aks-engine" {
+  name        = "aks-engine"
+  description = "Implement aks-engine"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository" "aws-connectivity" {
+  name        = "aws-connectivity"
+  description = "Terraform for aws-connectivity"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "github_repository" "aws-sandbox" {
+  name        = "aws-sandbox"
+  description = "Terraform for aws-sandbox"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
--- terraform-modules.tf
+++ terraform-modules.tf
@@ -19,3 +19,25 @@ resource "tfe_registry_module" "module-terraform_azurerm_aks_cluster" {
     oauth_token_id     = tfe_oauth_client.github_cloudruler.oauth_token_id
   }
 }
+
+resource "github_repository" "terraform_aws_k8s" {
+  name        = "terraform-aws-k8s"
+  description = "Module for creating an unmanaged k8s cluster on AWS"
+  auto_init   = true
+
+  template {
+    owner      = split("/", github_repository.terraform_module_repo_template.full_name)[0]
+    repository = github_repository.terraform_module_repo_template.name
+  }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+# resource "tfe_registry_module" "module-terraform_aws_k8s" {
+#   vcs_repo {
+#     display_identifier = "${var.github_organization}/${github_repository.terraform_aws_k8s.name}"
+#     identifier         = "${var.github_organization}/${github_repository.terraform_aws_k8s.name}"
+#     oauth_token_id     = tfe_oauth_client.github_cloudruler.oauth_token_id
+#   }
+# }
