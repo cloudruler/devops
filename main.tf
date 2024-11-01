d5248b6bbdd6c84c678c605699116bdecace5e10
Brian Moore <brianmoore@cloudruler.com>
2024-11-01 13:51:17 -0500

    Turned off container registry

--- main.tf
+++ main.tf
@@ -4,40 +4,9 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_container_registry" "cr" {
-  count               = 1
+  count               = 0
   name                = "cloudruler"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
 }
-
-import {
-  to = tfe_workspace.aws_connectivity
-  id = "cloudruler/aws-connectivity"
-}
-
-import {
-  to = tfe_workspace.aws_sandbox
-  id = "cloudruler/aws-sandbox"
-}
-
-import {
-  to = tfe_workspace.connectivity
-  id = "cloudruler/connectivity"
-}
-
-import {
-  to = tfe_workspace.devops
-  id = "cloudruler/devops"
-}
-
-import {
-  to = tfe_workspace.identity
-  id = "cloudruler/identity"
-}
-
-import {
-  to = tfe_workspace.sandbox
-  id = "cloudruler/sandbox"
-}
-
